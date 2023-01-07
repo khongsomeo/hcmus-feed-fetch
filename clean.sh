4f0b0a882c0e2e90f6504a7b8a18226f223cd7fe
hwangswan <mrthedzam@gmail.com>
2023-01-07 19:21:26 +0700

    Now important news will be mentioned in the terminal screen

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm feed.txt
+rm config/conf.json
--- feed_fetch.py
+++ feed_fetch.py
@@ -3,7 +3,8 @@ import json
 from datetime import datetime
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-CONF_FILE = "conf.json"
+CONF_FILE = "config/conf.json"
+KEYW_FILE = "config/keywords.txt"
 
 def main():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
@@ -17,6 +18,9 @@ def main():
 
     with open(args.urls, "r+", encoding="utf8") as f:
         urls_dict = json.load(f)
+
+    with open(KEYW_FILE, "r+", encoding="utf8") as f:
+        keys_list = [line.strip() for line in f.readlines()]
 
     try:
         with open(CONF_FILE, "r+") as f:
@@ -38,7 +42,11 @@ def main():
 
             print(f"Fetching {url_fullname} ({url})")
 
+            # Is this alias has a new news?
             has_news = False
+
+            # List of (need to focus) news.
+            focus_news = []
 
             feed_list = feedparser.parse(url)["entries"]
             for feed in feed_list:
@@ -54,10 +62,21 @@ def main():
                     print(f"Published on {feed_published}", file = f)
                     print("-" * 10, file = f)
 
-    if has_news is False:
-        print("No new news!")
-    else:
-        print(f"There are some new news, saved in {args.save}")
+                    for key in keys_list:
+                        if key.lower() in feed_title.lower():
+                            focus_news.append(feed_title)
+
+            if has_news is False:
+                print(f"No new news in {url_fullname}!")
+            else:
+                print(f"There are some new news in {url_fullname}, saved in {args.save}")
+
+                if len(focus_news) > 0:
+                    print(f"There are {len(focus_news)} important news:")
+                    for news in focus_news:
+                        print(f"- {news}")
+
+            print("-" * 10)
 
     conf_dict["last_update"] = current_date.__str__()
     with open(CONF_FILE, "w+") as f:
